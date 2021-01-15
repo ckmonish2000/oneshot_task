838264709cb2f5c8f1618865a1fdb7c10488bdcb
monish karunakaran <ckmonish2000>
2021-01-15 16:46:17 +0530

    refactored code

--- server/routers/college.js
+++ server/routers/college.js
@@ -3,6 +3,27 @@ let router=express.Router();
 let CollegeModel=require("../model/Model")
 
 
+
+
+
+// post
+router.post("/createcollege",(req,res)=>{
+    let body=req.body;
+    let create=new CollegeModel({
+        name:body.college_name,
+        yearFounded:body.date,
+        city:body.city,
+        state:body.state,
+        country:body.country,
+        No_Of_Students:body.No_Of_Students,
+        courses:body.courses
+    })
+
+    create.save()
+    .then(data=>res.status(200).json(data))
+    .catch(err=>{res.send(err)})
+})
+
 // get
 
 router.get("/",(req,res)=>{
@@ -45,23 +66,6 @@ router.get("/getByid",(req,res)=>{
 })
 
 
-// post
-router.post("/createcollege",(req,res)=>{
-    let body=req.body;
-    let create=new CollegeModel({
-        name:body.college_name,
-        yearFounded:body.date,
-        city:body.city,
-        state:body.state,
-        country:body.country,
-        No_Of_Students:body.No_Of_Students,
-        courses:body.courses
-    })
-
-    create.save()
-    .then(data=>res.status(200).json(data))
-    .catch(err=>{res.send(err)})
-})
 
 
 
