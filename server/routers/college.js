4ab40d502eba3fa346f418d6bafee57fe34328b7
monish karunakaran <ckmonish2000>
2021-01-15 16:43:56 +0530

    logic for get similar colleges by name and id and get all college end point

--- server/routers/college.js
+++ server/routers/college.js
@@ -4,16 +4,43 @@ let CollegeModel=require("../model/Model")
 
 
 // get
-router.get("/getByname",(req,res)=>{
-    CollegeModel.find({name:req.body.collegeName})
+
+router.get("/",(req,res)=>{
+    CollegeModel.find({})
     .then(data=>res.status(200).json(data))
+    .catch(err=>res.status(200).json(err))
+})
+
+router.get("/getByname",(req,res)=>{
+    let dta={}
+    CollegeModel.find({name:req.body.collegeName})
+    .then(data=>{
+        dta["main"]=data
+        // gets similar college
+        CollegeModel.find({city:data[0].city,courses:data[0].courses,name:{$ne:req.body.collegeName}})
+        .then(val=>{
+            dta["similar"]=val;
+            res.status(200).json(dta)
+        })
+        
+    })
     .catch(err=>res.json(err))
     
 })
 
 router.get("/getByid",(req,res)=>{
+    let dta={}
     CollegeModel.find({_id:req.body._id})
-    .then(data=>res.status(200).json(data))
+    .then(data=>{
+       dta["main"]=data;
+        // gets similar college
+        CollegeModel.find({city:data[0].city,courses:data[0].courses,_id:{$ne:req.body._id}})
+        .then(val=>{
+            dta["similar"]=val;
+            res.status(200).json(dta)
+        })
+    }
+        )
     .catch(err=>res.json(err))
 })
 
